b552a21b9774be2e0e71037629a36f018b65c849
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-20 13:01:13 +0900

    fixed bug

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -10,6 +10,8 @@ class UserTile extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+        padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(12),
@@ -17,6 +19,7 @@ class UserTile extends StatelessWidget {
         child: Row(
           children: [
             const Icon(Icons.person),
+            const SizedBox(width: 20),
             Text(text),
           ],
         ),
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -5,6 +5,10 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
   Future<UserCredential> signInWithEmailAndPassword(
       String email, password) async {
     try {
